1bddf465f57ad0d73fd2441696785c6e59e4f9f9
JhnRavelo <johnravelo135@gmail.com>
2024-03-04 10:04:08 +0300

    format code in axios api

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,15 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
 const defaultAxios = axios.create({
   withCredentials: true,
-})
+});
 
 const privateAxios = axios.create({
   withCredentials: true,
-  headers:{"Content-Type":'application/json'}
-})
+  headers: { "Content-Type": "application/json" },
+});
 
-export default defaultAxios
+export default defaultAxios;
 
-export {privateAxios}
-
+export { privateAxios };
