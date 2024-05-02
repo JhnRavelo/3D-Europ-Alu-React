c765fe35fec60e7e444331fa6dbc6cbf8faa2ccf
JhnRavelo <johnravelo135@gmail.com>
2024-05-02 14:01:58 +0300

    refactoring: handle change profil photo

--- src/components/Profils/Cart/Cart.jsx
+++ src/components/Profils/Cart/Cart.jsx
@@ -1,39 +1,32 @@
 import "./Cart.css";
 import propTypes from "prop-types";
-import { useEffect, useState } from "react";
 
-const Cart = ({data}) => {
-  const [product, setProduct] = useState([]);
-
-  useEffect(() => {
-    if (data?.traker.length != 0 ) {
-      setProduct(data.traker);
-    }
-  }, [data]);
-
+const Cart = ({ products }) => {
   return (
     <>
       <div className="pannier">
         <div className="titre__pannier">
           <h1>les produits qui vous ont intéressé:</h1>
         </div>
-        {product.map((prod, index) => (
-          <div className="cart-item" key={index}>
-            <div className="title__cart__item">
-              <h1>{prod.product.title}</h1>
+        {products &&
+          products?.length > 0 &&
+          products.map((product, index) => (
+            <div className="cart-item" key={index}>
+              <div className="title__cart__item">
+                <h1>{product.product.title}</h1>
+              </div>
+              <div className="img__cart__item">
+                <img src={product.product.png} alt="image du produit" />
+              </div>
             </div>
-            <div className="img__cart__item">
-              <img src={prod.product.png} alt="image du produit" />
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
 };
 
 Cart.propTypes = {
-  data: propTypes.any,
+  products: propTypes.any,
 };
 
 export default Cart;
--- src/components/Profils/UserProfileCard/UserProfileCard.jsx
+++ src/components/Profils/UserProfileCard/UserProfileCard.jsx
@@ -1,55 +1,45 @@
 import "./UserProfileCard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import avatars from "../../../assets/json/avatar.json";
 import propTypes from "prop-types";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
+import useAuth from "../../../hooks/useAuth";
 
-const UserProfileCard = ({ data, handleSetCart, handleSetChat }) => {
+const UserProfileCard = ({ setScreen }) => {
   const showChangeAvatarRef = useRef();
   const listeAvatarRef = useRef();
-  const [imgProfile, setImgProfile] = useState(avatars[0]);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+  const { auth, setAuth } = useAuth();
+  const [imgProfile, setImgProfile] = useState(
+    auth && auth?.avatar ? auth.avatar : avatars[0]
+  );
   const privateAxios = useAxiosPrivate();
 
   const showChangeAvatar = () => {
     listeAvatarRef.current.classList.toggle("showed");
   };
 
-  useEffect(() => {
-    if (data?.userRead?.length != 0) {
-      setEmail(data.userRead[0].email);
-      setPhone(data.userRead[0].phone);
-      setName(data.userRead[0].name);
-      setImgProfile(data.userRead[0].avatar);
-    }
-  }, [data]);
-
   const handleClick = (e) => {
-    if (e.target.src.includes("Avatar-Profile")) {
-      setImgProfile(avatars[1]);
-      handleFetch(1);
-    } else if (e.target.src.includes("user-profile")) {
-      setImgProfile(avatars[2]);
-      handleFetch(2);
-    } else if (e.target.src.includes("woman-business")) {
-      setImgProfile(avatars[3]);
-      handleFetch(3);
-    } else if (e.target.src.includes("woman-users")) {
-      setImgProfile(avatars[4]);
-      handleFetch(4);
-    }
+    avatars.map((avatar) => {
+      if (e.target.src.includes(avatar)) {
+        setImgProfile(avatar);
+        handleFetch(avatar);
+      }
+    });
   };
 
-  const handleFetch = async (index) => {
+  const handleFetch = async (avatar) => {
     try {
       await privateAxios.put("/auth/avatar", {
-        avatar: avatars[index],
+        avatar,
       });
-
+      setAuth((prev) => {
+        return {
+          ...prev,
+          avatar: avatar,
+        };
+      });
     } catch (error) {
       console.log(error);
     }
@@ -86,14 +76,14 @@ const UserProfileCard = ({ data, handleSetCart, handleSetChat }) => {
                 }
               })}
             </div>
-            <div className="name">{name}</div>
-            <div className="email">{email}</div>
-            <div className="number">{phone}</div>
+            <div className="name">{auth?.name}</div>
+            <div className="email">{auth?.email}</div>
+            <div className="number">{auth?.phone}</div>
           </div>
 
           <div className="buttons">
-            <button onClick={handleSetChat}>Message</button>
-            <button onClick={handleSetCart}>Pannier</button>
+            <button onClick={() => setScreen("chat")}>Message</button>
+            <button onClick={() => setScreen("cart")}>Pannier</button>
           </div>
         </div>
       </div>
@@ -103,8 +93,7 @@ const UserProfileCard = ({ data, handleSetCart, handleSetChat }) => {
 
 UserProfileCard.propTypes = {
   data: propTypes.any,
-  handleSetCart: propTypes.func,
-  handleSetChat: propTypes.func,
+  setScreen: propTypes.func,
 };
 
 export default UserProfileCard;
